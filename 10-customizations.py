7508c3537cbd05a961d38dac9b3b76c8e9364d34
MaxGosselin <max.jp.gosselin@gmail.com>
2018-12-05 01:46:58 -0500

    1-10

---
+++ 10-customizations.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import urllib
+import matplotlib.dates as mdates
+
+def bytespdate2num(fmt, encoding='utf-8'):
+    strconverter = mdates.strpdate2num(fmt)
+    def bytesconverter(b):
+        s = b.decode(encoding)
+        return strconverter(s)
+    return bytesconverter
+
+def graph_data():
+
+    fig = plt.figure()
+    ax1 = plt.subplot2grid((1, 1), (0, 0))
+
+    stock_price_url = 'https://pythonprogramming.net/yahoo_finance_replacement'
+    source_code = urllib.request.urlopen(stock_price_url).read().decode()
+    
+    stock_data = []
+    split_source = source_code.split('\n')
+    
+    for line in split_source:
+        split_line = line.split(',')
+        if len(split_line) == 7:
+            if 'Volume' not in line:
+                stock_data.append(line)
+    
+    date, openp, highp, lowp, closep, adjustedp, volume = np.loadtxt(stock_data,
+                                                                     delimiter=',',
+                                                                     unpack=True,
+                                                                     converters={0: bytespdate2num('%Y-%m-%d')})
+    ax1.plot_date(date, closep, '-', label='Price')
+
+    for label in ax1.xaxis.get_ticklabels():
+        label.set_rotation(45)
+    
+    ax1.grid(True) #, color='g', linestyle='-')
+    ax1.xaxis.label.set_color('c')
+    ax1.yaxis.label.set_color('r')
+
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Interesting graph\nCheck it out')
+    plt.legend()
+    plt.show()
+
+graph_data()    
